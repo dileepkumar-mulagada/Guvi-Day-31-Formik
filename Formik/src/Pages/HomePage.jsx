722bc18c7453a3dcbf02961d8aa42fc62731a082
dileepkumar-mulagada <162285455+dileepkumar-mulagada@users.noreply.github.com>
2024-07-09 22:04:37 +0530

    Add files via upload

---
+++ Formik/src/Pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <>
+    <div className="d-flex flex-column ml-1 justify-content-center align-items-center min-vh-100">
+      <h1 className="display-5 fw-bold lh-1 gradient-bg mb-5">Library Management System</h1>
+    </div>
+    </>
+  );
+};
+
+export default Homepage;
